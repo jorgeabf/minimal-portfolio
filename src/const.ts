84b1963e24ba195ab02bcbb77d4b86c2657e5e36
jorgeabf <aportuvas@outlook.es>
2024-02-01 01:23:17 +0100

    Update Astro configuration and add site URL
    Update favicon SVG
    
    Update index page with layout and sections
    
    Update tsconfig.json with paths

---
+++ src/const.ts
@@ -0,0 +1,5 @@
+const SITE_TITLE: string = 'Jorge Arce Web';
+const SITE_DESCRIPTION: string = 'Portfolio de Jorge Arce, Desarrollador Web Full Stack';
+const ACTUAL_YEAR: number = new Date().getFullYear();
+
+export { SITE_TITLE, SITE_DESCRIPTION, ACTUAL_YEAR };
