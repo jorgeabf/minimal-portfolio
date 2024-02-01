83b7bc235c11d966da311a37021b0b695f29cb02
jorgeabf <aportuvas@outlook.es>
2024-02-02 00:43:03 +0100

    Actualizar cv.json y Badge.astro, ajustar estilos y const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
-const SITE_TITLE: string = 'Jorge Arce Web';
-const SITE_DESCRIPTION: string = 'Portfolio de Jorge Arce, Desarrollador Web Full Stack';
+const SITE_TITLE: string = 'C.V. Jorge Arce Balluerca';
+const SITE_DESCRIPTION: string = 'Curriculum Vitae de Jorge Arce, Desarrollador Web Full Stack';
 const ACTUAL_YEAR: number = new Date().getFullYear();
 
 export { SITE_TITLE, SITE_DESCRIPTION, ACTUAL_YEAR };
